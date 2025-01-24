ffc539bf340a79d111353390385015049b5b170f
amandahandrade <amandah.andrades@gmail.com>
2025-01-24 16:22:11 -0300

    fix: corrige lógica de troca de cores do semáforo com useState, torna o layout responsivo e atualiza o título do site
    - Ajustada a lógica de troca de cores do semáforo ao clicar nos botões utilizando useState.
    - Tornado o layout responsivo para se ajustar a diferentes dispositivos.
    - Removida a palavra 'teste' do título e adicionada uma imagem ao site.
    - Removidos ícones desnecessários (react.svg, vite.svg).
    - Adicionada nova logo (logo.png) na pasta de assets.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,52 @@ import { useEffect, useState } from "react";
 import s from "./app.module.scss";
 
 export default function App() {
+  const [cor, setCor] = useState("luz");
+
+  const mudarCor = (novaCor) => {
+    const coresValidas = ["vermelho", "amarelo", "verde"];
+    // Verifica se a nova cor passada é uma das cores válidas
+    if (coresValidas.includes(novaCor)) {
+      // Se for válida, atualiza o estado 'cor' com a nova cor
+      setCor(novaCor);
+    }
+  };
+
+  // Outra forma de fazer:
+  // const mudarCor = (novaCor) => {
+  //   if (novaCor === "vermelho") {
+  //     setCor("vermelho");
+  //   } else if (novaCor === "amarelo") {
+  //     setCor("amarelo");
+  //   } else if (novaCor === "verde") {
+  //     setCor("verde");
+  //   }
+  // };
+
   return (
     <main>
       <section className={s.semaforo}>
-        <div className={s.luz}></div>
-        <div className={s.luz}></div>
-        <div className={s.luz}></div>
+        {/* Luz vermelha: se o estado 'cor' for 'vermelho', aplica a classe 's.vermelho', senão usa a classe 'luz' */}
+        {/* condição ? valorSeVerdadeiro : valorSeFalso; */}
+        <div
+          className={`${s.luz} ${cor === "vermelho" ? s.vermelho : "luz"}`}
+        ></div>
+        <div
+          className={`${s.luz} ${cor === "amarelo" ? s.amarelo : "luz"}`}
+        ></div>
+        <div className={`${s.luz} ${cor === "verde" ? s.verde : "luz"}`}></div>
       </section>
 
       <section className={s.btn}>
-        <button className={s.btnVerde}>Verde</button>
-        <button className={s.btnAmarelo}>Amarelo</button>
-        <button className={s.btnVermelho}>Vermelho</button>
+        <button className={s.btnVermelho} onClick={() => mudarCor("vermelho")}>
+          Vermelho
+        </button>
+        <button className={s.btnAmarelo} onClick={() => mudarCor("amarelo")}>
+          Amarelo
+        </button>
+        <button className={s.btnVerde} onClick={() => mudarCor("verde")}>
+          Verde
+        </button>
       </section>
     </main>
   );
